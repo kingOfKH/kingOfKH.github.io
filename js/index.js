3dd2e9e5217b8d2562b141954317d055a1e12ebe
kingOfKH <3515884990@qq.com>
2024-03-05 13:02:44 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,23 +1,14 @@
-// 创建 XMLHttpRequest 对象
-var xhr = new XMLHttpRequest();
+var url = "../test.json"
+// 申明一个XMLHttpRequest
+var request = new XMLHttpRequest();
+// 设置请求方法与路径
+request.open("get", url);
+// 不发送数据到服务器
+request.send(null);
+//XHR对象获取到返回信息后执行
+request.onload = function () {
+    // 解析获取到的数据
+    var data = JSON.parse(request.responseText);
+    console.log(data)
 
-// 配置请求，true 表示异步请求
-xhr.open('GET', 'https://gitcode.net/2301_78006839/wxpj/-/raw/master/test.json', true);
-
-// 设置请求完成时的回调函数
-xhr.onreadystatechange = function() {
-    // 检查请求是否完成
-    if (xhr.readyState == 4) {
-        // 检查 HTTP 状态码
-        if (xhr.status == 200) {
-            // 请求成功，你可以在 xhr.responseText 中获取响应的数据
-            console.log(xhr.responseText);
-        } else {
-            // 请求失败，输出错误信息
-            console.error('Failed to fetch data. Status: ' + xhr.status);
-        }
-    }
-};
-
-// 发送请求
-xhr.send();
+}
