85623fa1aab846f1f899cefa57d3a1d78f4e559a
KingIsGD <2237557491@qq.com>
2024-04-01 22:58:29 +0800

    240401

--- component/slider/slider.js
+++ component/slider/slider.js
@@ -54,18 +54,33 @@ timer = setInterval(nextSlide, 2000); // 设置初始定时器
 
 let touchStartX = 0;
 let touchEndX = 0;
+let touchMoveX = 0;
+
+let touchStartY = 0;
+let touchMoveY = 0;
 
 slides.addEventListener('touchstart', (e) => {
   touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
   clearInterval(timer); // 清除之前的定时器
   
 });
 
 slides.addEventListener('touchmove', (e) => {
   touchMoveX = e.touches[0].clientX;
-  const offsetX =  touchStartX - touchMoveX;
+  touchMoveY = e.touches[0].clientY;
+  const offsetX = touchStartX - touchMoveX;
+  const offsetY = touchStartY - touchMoveY;
+  // 判断是否水平滚动
+  if (Math.abs(offsetX) > Math.abs(offsetY)) {
+    e.preventDefault(); // 只阻止默认的触摸事件行为，当水平滚动时
+  }
   slides.style.transform = `translateX(-${slideWidth * currentSlide + offsetX}px)`;
-  
+  // 判断触摸移动过程中是否超出幻灯片范围，如果超出则立即阻止默认事件
+  const rect = slides.getBoundingClientRect();
+  if (touchMoveX < rect.left || touchMoveX > rect.right || touchMoveY < rect.top || touchMoveY > rect.bottom) {
+    e.preventDefault();
+  }
 });
 
 slides.addEventListener('touchend', (e) => {
